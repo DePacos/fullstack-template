073b156ce3ec86ab0fff44b3415d6a68da1f43a4
depacos <depacos@yandex.ru>
2025-12-24 17:32:27 +0300

    fix(api): app module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,21 +1,24 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { TokensModule } from '@/auth/tokens';
-import { PrismaService } from '@/prisma';
+import { PrismaModule } from '@/prisma';
 import { UserModule } from '@/user';
 
+import { AuthModule } from '@/auth/auth.module';
 import { MailModule } from '@/mail/mail.module';
 import { TrpcModule } from '@/trpc/trpc.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
     MailModule,
     TrpcModule,
+    PrismaModule,
     TokensModule,
     UserModule,
   ],
-  providers: [ConfigService, PrismaService],
+  providers: [ConfigService],
 })
 export class AppModule {}
