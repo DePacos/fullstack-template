9a0a9d58e7305f5ff1105e4e2f218ebee0500dff
depacos <depacos@yandex.ru>
2025-12-25 17:39:20 +0300

    feat(web): icons

---
+++ packages/ui/src/assets/icons/svg/index.ts
@@ -0,0 +1,6 @@
+export { default as GoogleIcon } from './google.icon.svg';
+export { default as LoaderIcon } from './loader.icon.svg';
+export { default as InfoIcon } from './info.icon.svg';
+export { default as CircleCheckIcon } from './circle-check.icon.svg';
+export { default as OctagonIcon } from './octagon.icon.svg';
+export { default as TriangleAlertIcon } from './triangle-alert.icon.svg';
